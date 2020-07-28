071c9898cc366a20ce1ffec3cb6f2db1dceac92a
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-07-27 22:03:29 -0300

    Button PENDENTE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Button>
+        Olá
+      </Button>
     </div>
   );
 }
